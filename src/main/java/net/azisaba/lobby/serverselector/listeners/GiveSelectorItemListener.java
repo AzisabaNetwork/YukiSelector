e9b4799ea71e56b0e85ebf5104b3d3e9e2ffb7ac
siloneco <siloneco4646@gmail.com>
2019-08-02 22:29:47 +0900

    [Add] サーバー退出時にサーバー選択アイテムを消すリスナーを追加

--- src/main/java/net/azisaba/lobby/serverselector/listeners/GiveSelectorItemListener.java
+++ src/main/java/net/azisaba/lobby/serverselector/listeners/GiveSelectorItemListener.java
@@ -4,6 +4,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
 
 import lombok.RequiredArgsConstructor;
 import net.azisaba.lobby.serverselector.ServerSelector;
@@ -30,5 +32,25 @@ public class GiveSelectorItemListener implements Listener {
 
         // アイテムをセット
         p.getInventory().setItem(4, plugin.getSelectorItem());
+    }
+
+    /**
+     * プレイヤー退出時にサーバー選択アイテムを削除するListener <br>
+     * Pluginを抜いた後にプレイヤーデータをいじってアイテムを削除しなければならなくなるのを防ぐため
+     * @param e
+     */
+    @EventHandler
+    public void onQuit(PlayerQuitEvent e) {
+        Player p = e.getPlayer();
+        // スロット4のアイテムを取得
+        ItemStack item = p.getInventory().getItem(4);
+
+        // nullならreturn
+        if ( item == null ) {
+            return;
+        }
+        if ( item.isSimilar(plugin.getSelectorItem()) ) {
+            p.getInventory().clear(4);
+        }
     }
 }
